12b7d7e94a48b3a656e87e6b94e9c50dd8a60112
msoyucok <muhammet.soyucok@icloud.com>
2026-03-05 23:18:25 +0100

    chore: add eslint config for ui, fix lint blocking in CI

---
+++ packages/ui/eslint.config.js
@@ -0,0 +1,20 @@
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+
+export default [
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+]
