cf0427ca27b4d8556e1cf13e2585775db73dd6a8
msoyucok <muhammet.soyucok@icloud.com>
2026-03-11 10:42:29 +0100

    Remove Vela project contents

--- packages/ui/src/components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Card({ className = '', children }: React.PropsWithChildren<{ className?: string }>) {
-  return (
-    <div className={`rounded-[14px] border p-5 bg-[var(--surface-1)] border-[var(--border)] ${className}`}>
-      {children}
-    </div>
-  )
-}
--- packages/ui/src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from './Button'
-export { Card } from './Card'
-export { Input } from './Input'
-export { SegmentedControl } from './SegmentedControl'
